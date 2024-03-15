009d742ad3fb3ff6076fad949063ce9a5428f900
Ivan <>
2024-03-15 17:20:25 +0100

    four

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -7,29 +7,40 @@ package javaapplication1;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class JavaApplication1 {
     public static void main(String[] args) {
-        double[] arguments = {30, 45, 60, 90};
+          Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of arguments:");
+        int numArgs = scanner.nextInt();
+        double[] arguments = new double[numArgs];
+        System.out.println("Enter arguments:");
+        for (int i = 0; i < numArgs; i++) {
+            arguments[i] = scanner.nextDouble();
+        }
+
+        System.out.println("Choose result display format:");
+        System.out.println("1. Text Format");
+        System.out.println("2. Table Format");
+        int choice = scanner.nextInt();
+
+        ResultFactory resultFactory;
+        if (choice == 1) {
+            System.out.println("Enter column width:");
+            int columnWidth = scanner.nextInt();
+            resultFactory = new TextResultFactory(columnWidth);
+        } else {
+            System.out.println("Enter number of columns:");
+            int columns = scanner.nextInt();
+            resultFactory = new TableResultFactory(columns);
+        }
 
         int onesCount = CalculationManager.countOnes(arguments);
-
         ResultData resultData = new ResultData(arguments, onesCount);
-        try {
-            StateManager.saveObject("result.dat", resultData);
-            ResultData restoredResult = (ResultData) StateManager.restoreObject("result.dat");
 
-            ResultFactory resultFactory = new TextResultFactory();
-            ResultDisplay resultDisplay = resultFactory.createResultDisplay();
-
-            System.out.println("Before serialization:");
-            resultDisplay.display(resultData);
-
-            System.out.println("\nAfter deserialization:");
-            resultDisplay.display(restoredResult);
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        ResultDisplay resultDisplay = resultFactory.createResultDisplay();
+        resultDisplay.display(resultData);
     }
 }
 
@@ -38,23 +49,50 @@ interface ResultDisplay {
 }
 
 class TextResultDisplay implements ResultDisplay {
+    private int columnWidth;
+
+    public TextResultDisplay(int columnWidth) {
+        this.columnWidth = columnWidth;
+    }
+
     @Override
     public void display(ResultData result) {
-        System.out.println("Arguments: ");
-        for (double arg : result.getArguments()) {
-            System.out.print(arg + " ");
+        System.out.println("Arguments:");
+        for (int i = 0; i < result.getArguments().length; i++) {
+            System.out.printf("%-" + columnWidth + "s", result.getArguments()[i]);
+            if ((i + 1) % 3 == 0) {
+                System.out.println();
+            }
         }
         System.out.println("\nOnes Count in Binary Representation of Average: " + result.getOnesCount());
     }
 }
 
-class CalculationManager implements Serializable {
-    private static final long serialVersionUID = 2L;
+class TableResultDisplay implements ResultDisplay {
+    private int columns;
 
+    public TableResultDisplay(int columns) {
+        this.columns = columns;
+    }
+
+    @Override
+    public void display(ResultData result) {
+        System.out.println("Arguments Table:");
+        for (int i = 0; i < result.getArguments().length; i++) {
+            System.out.printf("%-15s", result.getArguments()[i]);
+            if ((i + 1) % columns == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println("\nOnes Count in Binary Representation of Average: " + result.getOnesCount());
+    }
+}
+
+class CalculationManager {
     public static int countOnes(double... args) {
         double sum = 0;
         for (double arg : args) {
-            sum += Math.sin(arg)*1000;
+            sum += Math.sin(Math.toRadians(arg));
         }
         double avg = sum / args.length;
         int integerPart = (int) avg;
@@ -69,9 +107,7 @@ class CalculationManager implements Serializable {
     }
 }
 
-class ResultData implements Serializable {
-    private static final long serialVersionUID = 1L;
-
+class ResultData {
     private double[] arguments;
     private int onesCount;
 
@@ -94,22 +130,27 @@ interface ResultFactory {
 }
 
 class TextResultFactory implements ResultFactory {
+    private int columnWidth;
+
+    public TextResultFactory(int columnWidth) {
+        this.columnWidth = columnWidth;
+    }
+
     @Override
     public ResultDisplay createResultDisplay() {
-        return new TextResultDisplay();
+        return new TextResultDisplay(columnWidth);
     }
 }
 
-class StateManager {
-    public static void saveObject(String filename, Serializable obj) throws IOException {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
-            oos.writeObject(obj);
-        }
+class TableResultFactory implements ResultFactory {
+    private int columns;
+
+    public TableResultFactory(int columns) {
+        this.columns = columns;
     }
 
-    public static Object restoreObject(String filename) throws IOException, ClassNotFoundException {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
-            return ois.readObject();
-        }
+    @Override
+    public ResultDisplay createResultDisplay() {
+        return new TableResultDisplay(columns);
     }
 }
